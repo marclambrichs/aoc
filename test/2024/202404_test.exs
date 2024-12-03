ea2994db73fe1032ca78248ae7ae3d787afd4a7e
Marc Lambrichs <mlambrichs@melange-it.nl>
2024-12-03 16:28:58 +0100

    feat: Add day 4 part 1

---
+++ test/2024/202404_test.exs
@@ -0,0 +1,15 @@
+defmodule Aoc202404_test do
+  use ExUnit.Case
+
+  import Aoc202404
+
+  describe "part1/0" do
+    test "returns" do
+    end
+  end
+
+  describe "part2/0" do
+    test "returns" do
+    end
+  end
+end
