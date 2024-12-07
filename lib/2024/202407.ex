b3da7ae10b84180bfd697d7e790a9118f14edab3
Marc Lambrichs <mlambrichs@melange-it.nl>
2024-12-07 14:10:51 +0100

    feat: Add day 7 part 1

---
+++ lib/2024/202407.ex
@@ -0,0 +1,35 @@
+defmodule Aoc202407 do
+  def run() do
+    IO.inspect(part1())
+    IO.inspect(part2())
+  end
+
+  def part1() do
+    calibrations()
+    |> Enum.filter(fn {key, values} ->
+      key in calculate(values)
+    end)
+    |> Enum.map(&elem(&1, 0))
+    |> Enum.sum()
+  end
+
+  def part2() do
+  end
+
+  def calibrations() do
+    for lines <- Aoc.input_lines(__MODULE__) |> Enum.to_list(), into: %{} do
+      [key, values] = String.split(lines, ": ")
+      {String.to_integer(key), values |> String.split(" ") |> Enum.map(&String.to_integer/1)}
+    end
+  end
+
+  def calculate(list) do
+    Enum.reduce(list, fn
+      x, acc when is_list(acc) ->
+        Enum.map(acc, &[&1 * x, &1 + x]) |> List.flatten()
+
+      x, acc ->
+        [acc * x, acc + x]
+    end)
+  end
+end
