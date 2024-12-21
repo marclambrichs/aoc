2c4f25af21176d4bf81da00ebc312f49a747a56c
Marc Lambrichs <mlambrichs@melange-it.nl>
2024-12-21 12:12:11 +0100

    feat: Add day 12 part 2

--- lib/2024/202412.ex
+++ lib/2024/202412.ex
@@ -15,7 +15,15 @@ defmodule Aoc202412 do
   end
 
   def part2() do
-    example()
+    garden = garden(example(0))
+
+    partitions(garden)
+    |> Enum.map(fn region ->
+      area(region) *
+        (Enum.map(region, &(corners(:inner, garden, &1) + corners(:outer, garden, &1)))
+         |> Enum.sum())
+    end)
+    |> Enum.sum()
   end
 
   def example(nr \\ 0) do
@@ -49,6 +57,16 @@ defmodule Aoc202412 do
         """
         |> String.trim()
         |> String.split("\n")
+
+      3 ->
+        """
+        AAAA
+        BBCD
+        BBCC
+        EEEC
+        """
+        |> String.trim()
+        |> String.split("\n")
     end
     |> Enum.map(&String.split(&1, "", trim: true))
   end
@@ -58,7 +76,7 @@ defmodule Aoc202412 do
 
   def garden(matrix) do
     Enum.reduce(0..(length(matrix) - 1), %{}, fn row, acc ->
-      Enum.reduce(0..(length(matrix) - 1), acc, fn col, acc ->
+      Enum.reduce(0..(length(Enum.at(matrix, row)) - 1), acc, fn col, acc ->
         Map.put(acc, {row, col}, type({row, col}, matrix))
       end)
     end)
@@ -100,4 +118,33 @@ defmodule Aoc202412 do
   end
 
   ########## Part 2 ##########
+  def corners(:outer, garden, {a, b}) do
+    [{{0, -1}, {-1, 0}}, {{-1, 0}, {0, 1}}, {{0, 1}, {1, 0}}, {{1, 0}, {0, -1}}]
+    |> Enum.map(fn {{a1, b1}, {a2, b2}} -> {{a + a1, b + b1}, {a + a2, b + b2}} end)
+    |> Enum.map(fn {x, y} ->
+      Map.get(garden, x) != Map.get(garden, {a, b}) and
+        Map.get(garden, y) != Map.get(garden, {a, b})
+    end)
+    |> Enum.filter(& &1)
+    |> Enum.count()
+  end
+
+  def corners(:inner, garden, {a, b}) do
+    [
+      {{0, -1}, {-1, 0}, {-1, -1}},
+      {{-1, 0}, {0, 1}, {-1, 1}},
+      {{0, 1}, {1, 0}, {1, 1}},
+      {{1, 0}, {0, -1}, {1, -1}}
+    ]
+    |> Enum.map(fn {{a1, b1}, {a2, b2}, {a3, b3}} ->
+      {{a + a1, b + b1}, {a + a2, b + b2}, {a + a3, b + b3}}
+    end)
+    |> Enum.map(fn {x, y, diag} ->
+      Map.get(garden, x) == Map.get(garden, {a, b}) and
+        Map.get(garden, y) == Map.get(garden, {a, b}) and
+        Map.get(garden, diag) != Map.get(garden, {a, b})
+    end)
+    |> Enum.filter(& &1)
+    |> Enum.count()
+  end
 end
